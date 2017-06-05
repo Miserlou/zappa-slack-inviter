77011d7f8a8676f57211532eb02cff86e8333806
Mitchell Grice <mitchell.grice@polymathian.com>
2017-06-05 10:11:35 +1000

    Flask standard. PEP8

--- app.py
+++ app.py
@@ -18,30 +18,33 @@ SLACK_DOMAIN = "zappateam.slack.com"
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
 def hello():
-    if request.method == 'POST':
+    return render_template(
+        'hello.html',
+        team_name=TEAM_NAME,
+    )
 
-        email = request.form['email']
-        data = {
-            'email': email,
-            'token': SLACK_TOKEN,
-            'set_active': True
-        }
-        url = 'https://' + SLACK_DOMAIN + '/api/users.admin.invite'
 
-        resp = requests.post(url, data=data)
-        if resp.status_code == 200:
-            return render_template('success.html', 
-                    team_name=TEAM_NAME,
-                )
-        else:
-            return render_template('failure.html', 
-                    team_name=TEAM_NAME,
-                )
+@app.route('/', methods=['POST'])
+def join():
+    email = request.form['email']
+    data = {
+        'email': email,
+        'token': SLACK_TOKEN,
+        'set_active': True
+    }
+    url = 'https://' + SLACK_DOMAIN + '/api/users.admin.invite'
 
+    resp = requests.post(url, data=data)
+    if resp.status_code == 200:
+        return render_template(
+            'success.html',
+            team_name=TEAM_NAME,
+        )
     else:
-        return render_template('hello.html', 
-                team_name=TEAM_NAME,
-
-            )
+        return render_template(
+            'failure.html',
+            team_name=TEAM_NAME,
+        )
